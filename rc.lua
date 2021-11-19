8c9cd555fc720e210fc0fbfac3c785968d0eae43
fwar34 <fwar34@126.com>
2021-11-19 23:38:47 +0800

    update

--- rc.lua
+++ rc.lua
@@ -695,6 +695,15 @@ for i = 1, 9 do
                         end
                   end,
                   {description = "view tag #"..i, group = "tag"}),
+        awful.key({ altkey }, "#" .. i + 9,
+                  function ()
+                        local screen = awful.screen.focused()
+                        local tag = screen.tags[i]
+                        if tag then
+                           tag:view_only()
+                        end
+                  end,
+                  {description = "view tag #"..i, group = "tag"}),
         -- Toggle tag display.
         awful.key({ modkey, "Control" }, "#" .. i + 9,
                   function ()
