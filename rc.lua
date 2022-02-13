237b08d49990725b660ad71a79cc84f3640f4c6b
fwar34 <fwar34@126.com>
2022-02-13 10:27:31 +0800

    add key bindings

--- rc.lua
+++ rc.lua
@@ -348,6 +348,19 @@ globalkeys = mytable.join(
         end,
         {description = "focus previous by index", group = "client"}
     ),
+    -- Default client focus
+    awful.key({ modkey,           }, "j",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
+    awful.key({ modkey,           }, "k",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
 
     -- By-direction client focus
     awful.key({ modkey }, "j",
